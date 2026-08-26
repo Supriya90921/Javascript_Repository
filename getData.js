84a04c5b5c1a7652cfb3ffbad3d24cac3a3847c6
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2026-08-26 23:59:01 +0530

    Refactor getData to use async/await
    Refactor getData function to use async/await for fetching data.

--- getData.js
+++ getData.js
@@ -1,5 +1,5 @@
 // fetch the data using promise and then 
-// fetch the data using promise and then 
+
 function getData()
 {
   console.log("Loading...");
@@ -16,5 +16,30 @@ function getData()
           {
             console.log("Error " + error.message);
   });
+}
+getData();
+
+
+//Same function using async await keywords
+
+async function getData()
+{
+  console.log("Loading...");
+try
+{
+  
+  const res=await fetch ("https://jsonplaceholder.typicode.com/users");
+  const data=await res.json();
+
+  data.forEach((user) =>
+    {
+      console.log("user name" + " "+ user.name);
+    });
+}
+
+  catch(error)
+    {
+      console.log("Error " + error.message);
+    };
 }
 getData();
